a77f9f2691c7f932b6c6972e53518b186be4d558
Venkat <vanakkamvenkat@zoho.com>
2017-07-01 23:53:32 +0530

    third line added in print out

--- redbus-sanity/src/test/java/redbusWebsiteCalls/HomePageCalls.java
+++ redbus-sanity/src/test/java/redbusWebsiteCalls/HomePageCalls.java
@@ -3,11 +3,12 @@ package redbusWebsiteCalls;
 import org.testng.annotations.Test;
 
 public class HomePageCalls {
-  @Test
+  @Test (priority=0)
   public void f() {
 	  for (int i = 0 ; i < 10 ; i++) {
 	  System.out.println("Hello World");
 	  System.out.println("Additional code");
+	  System.out.println("Third time");
 	  }
   }
 }
