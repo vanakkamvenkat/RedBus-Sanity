cd6754d53bf7fe3d0d2357aad927594a4d4c6236
Venkat <vanakkamvenkat@zoho.com>
2017-07-06 23:37:52 +0530

    Setup testng.xml to accept browser choice at time of value input

--- redbus-sanity/src/test/java/genericCalls/GenericCalls.java
+++
@@ -1,5 +0,0 @@
-package genericCalls;
-
-public class GenericCalls {
-
-}
--- redbus-sanity/src/test/java/redbusWebsiteCalls/HomePageCalls.java
+++ redbus-sanity/src/test/java/redbusWebsiteCalls/HomePageCalls.java
@@ -1,14 +1,48 @@
 package redbusWebsiteCalls;
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import genericCalls.GenCalls;
 
-public class HomePageCalls {
-  @Test (priority=0)
-  public void f() {
-	  for (int i = 0 ; i < 10 ; i++) {
-	  System.out.println("Hello World");
-	  System.out.println("Additional code");
-	  System.out.println("Third time");
-	  }
-  }
+public class HomePageCalls extends GenCalls {
+	@Test(priority = 0)
+	public void f() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello World");
+			System.out.println("Additional code");
+			System.out.println("Third time");
+		}
+	}
+
+	@Parameters("browser")
+	@BeforeSuite
+	public void before(String browser) { 
+		switch (browser.toLowerCase()) {
+		case "chrome":			  
+			chrome();
+			break;
+
+		case "firefox":
+			firefox();
+			break;
+
+		case "ie":
+			iexplore();
+			break;
+			
+		case "edge":
+			edge();
+			break;
+		}
+
+		driver.get("http://www.google.com");
+	}
+
+	@AfterSuite
+	public void after() {
+		driver.quit();
+	}
+
 }
